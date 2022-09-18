d317ea3fe9b284dc4fd0275595dac74c3488ca06
mert <mert.ma346@gmail.com>
2022-09-18 15:32:35 +0300

    Initial Commit

---
+++ swiftUI-widgetKit/swiftUI_widgetKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUI_widgetKitApp.swift
+//  swiftUI-widgetKit
+//
+//  Created by Mert AKBAŞ on 18.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftUI_widgetKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
