d79c699a21a6ebbbca04d9dbc97c55e00902d54b
mert <mert.ma346@gmail.com>
2022-09-18 16:40:19 +0300

    App Storage oluşturuldu.

--- swiftUI-widgetKit/View/ContentView.swift
+++ swiftUI-widgetKit/View/ContentView.swift
@@ -8,13 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("ankara", store: UserDefaults(suiteName: "group.mert.swiftUI-widgetKit"))
+    var ankaraData : Data = Data()
     var body: some View {
         VStack {
             ForEach(sehirlerDizisi) { ankara in
-                AnkaraView(ankara: ankara)
+                AnkaraView(ankara: ankara).onTapGesture {
+                    userDefaultsKaydet(ankara: ankara)
+                }
             }
         }
         
+    }
+    
+    func userDefaultsKaydet(ankara: Sehirler){
+       
+        
+        if let ankaraData = try? JSONEncoder().encode(ankara){
+            self.ankaraData = ankaraData
+            print(ankara.isim)
+        }
     }
 }
 
